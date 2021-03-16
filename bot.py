916a5bef74ecd1c063f1c6f48e36fbc0817d65b3
Shina <shinadan11@gmail.com>
2021-03-16 16:33:55 +0200

    bot updated

--- bot.py
+++ bot.py
@@ -6,17 +6,33 @@ Created on Sat Mar  6 15:01:31 2021
 """
 import time
 from scrapers import tds, mds, mml, mai
+from twitter import Twitter, OAuth, TwitterHTTPError
+import credentials
+
+oauth = OAuth(
+        credentials.ACCESS_TOKEN,
+        credentials.ACCESS_SECRET,
+        credentials.CONSUMER_KEY,
+        credentials.CONSUMER_SECRET)
+twit = Twitter(auth=oauth)
 
 def data_articles():
     print('Bot starting')
     webpages = ['tds', 'mds', 'mml', 'mai']
     for webpage in webpages:
         for article in globals()[webpage]():
-            tweet = article
-            print(tweet , '\n')
-            time.sleep(0)
+            try:
+                tweet = article
+                twit.statuses.update(status=tweet)
+                #print(tweet , '\n')
+                time.sleep(60)
+            except TwitterHTTPError:
+                continue
             
 if __name__ == '__main__':
-    data_articles()
+    try:
+        data_articles()
+    except Exception as e:
+        print(e)
     
 
